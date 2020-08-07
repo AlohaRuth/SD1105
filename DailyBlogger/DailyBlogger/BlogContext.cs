b8f03d47058c1e5347932bc847a67c6203f1f256
AlohaRuth <64048368+AlohaRuth@users.noreply.github.com>
2020-08-06 19:19:54 -0500

    Daily blogger in

---
+++ DailyBlogger/DailyBlogger/BlogContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DailyBlogger.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyBlogger
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<BlogPost> blogPost { get; set; }
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+
+        }
+    }
+}
