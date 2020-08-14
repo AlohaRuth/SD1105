b6947b1799c852ad621a1b4ae33717af7918ce8a
AlohaRuth <64048368+AlohaRuth@users.noreply.github.com>
2020-08-14 16:28:43 -0500

    Updating

---
+++ Sakila2/Sakila2/Models/Countrylanguage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sakila2.Models
+{
+    public partial class Countrylanguage
+    {
+        public string CountryCode { get; set; }
+        public string Language { get; set; }
+        public string IsOfficial { get; set; }
+        public decimal Percentage { get; set; }
+    }
+}
